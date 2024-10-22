92ce9659300ed983baf638800b79dff72cbf3630
AmaanGrofers <amaan.ahmad@grofers.com>
2024-10-22 11:50:09 +0530

    added empty state

---
+++ src/common/EmptyState/index.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types */
+import Title from "antd/es/typography/Title";
+import EmptyBox from "./empty-box.png";
+
+function EmptyState({
+  height = 200,
+  width = 200,
+  flexDirection = "col",
+  title = "Data not found",
+  titleSize = 5,
+}) {
+  const dirn = `flex-${flexDirection}`;
+
+  return (
+    <div className={`flex ${dirn} items-center justify-center text-slate-100`}>
+      <div>
+        <img
+          src={EmptyBox}
+          alt="empty-state"
+          className="object-contain p-4"
+          height={height}
+          width={width}
+        />
+      </div>
+      <Title level={titleSize} style={{ color: "#4f4f4f" }}>
+        {title}
+      </Title>
+    </div>
+  );
+}
+
+export default EmptyState;
